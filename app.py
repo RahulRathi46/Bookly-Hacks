5ba6ec50453f868a6fe2ef9681d69b70e024ecfa
Rahul Rathi <33680151+RahulRathi46@users.noreply.github.com>
2024-03-08 13:02:58 +0530

    init

---
+++ app.py
@@ -0,0 +1,133 @@
+from flask import Flask, jsonify, request
+import re
+import random
+import requests
+import json
+from bs4 import BeautifulSoup
+
+app = Flask(__name__)
+
+user_agents = [
+    # Windows User-Agents
+    'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.102 Safari/537.36',
+    'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/95.0.4638.69 Safari/537.36',
+    'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36',
+    'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.198 Safari/537.36',
+    'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.116 Safari/537.36',
+
+    # Mac User-Agents
+    'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.102 Safari/537.36',
+    'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/95.0.4638.69 Safari/537.36',
+    'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36',
+    'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.198 Safari/537.36',
+    'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.116 Safari/537.36',
+
+    # Linux User-Agents
+    'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:98.0) Gecko/20100101 Firefox/98.0',
+    'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:95.0) Gecko/20100101 Firefox/95.0',
+    'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:91.0) Gecko/20100101 Firefox/91.0',
+    'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:86.0) Gecko/20100101 Firefox/86.0',
+    'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:83.0) Gecko/20100101 Firefox/83.0',
+
+    # Android User-Agents
+    'Mozilla/5.0 (Linux; Android 11; SM-G991U) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.101 Mobile Safari/537.36',
+    'Mozilla/5.0 (Linux; Android 11; SM-G991U) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/95.0.4638.69 Mobile Safari/537.36',
+    'Mozilla/5.0 (Linux; Android 11; SM-G991U) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Mobile Safari/537.36',
+    'Mozilla/5.0 (Linux; Android 11; SM-G991U) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.198 Mobile Safari/537.36',
+    'Mozilla/5.0 (Linux; Android 11; SM-G991U) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.106 Mobile Safari/537.36',
+]
+
+
+@app.route('/get_booking_details', methods=['POST'])
+def get_booking_details():
+    # Get JSON data from the request
+    request_data = request.json
+
+    # Extract data from JSON
+    therapist_id = request_data.get('therapist_id')
+    loc_id = request_data.get('loc_id')
+    service_id = request_data.get('service_id')
+    auth_token = request_data.get('auth_token')
+    base_url = request_data.get('base_url')
+
+    # Check if all required fields are present
+    if not therapist_id or not loc_id or not service_id or not auth_token or not base_url:
+        return jsonify({'error': 'Missing required fields in JSON data'}), 400
+
+    # URL of the slots page
+    url = f'{base_url}?therapiest_id={therapist_id}&location={loc_id}&therapy_for_id={service_id}'
+
+    # Randomly select a User-Agent header
+    user_agent = random.choice(user_agents)
+
+    # Define the User-Agent header using the randomly selected User-Agent string
+    headers = {
+        'User-Agent': user_agent
+    }
+
+    # Create a session to handle cookies
+    with requests.Session() as session:
+        # Send a GET request to the slots page with the User-Agent header
+        response = session.get(url, headers=headers)
+
+        # Check if the request was successful
+        if response.ok:
+            # Parse the HTML content of the page
+            soup = BeautifulSoup(response.text, 'html.parser')
+
+            # Find the div with an ID containing the pattern 'bookly-form-'
+            pattern = re.compile(r'bookly-form-\w+')
+            bookly_form = soup.find('div', id=pattern)
+
+            if bookly_form:
+                bookly_form_id = bookly_form['id']
+            else:
+                bookly_form_id = None
+
+            # Get PHPSESSID cookie value
+            phpsessid_cookie = session.cookies.get('PHPSESSID')
+
+            # Find the <script> tag with ID "bookly-globals-js-extra"
+            script_tag = soup.find('script', id='bookly-globals-js-extra')
+
+            if script_tag:
+                # Extract the JavaScript content from the <script> tag
+                script_content = script_tag.string
+
+                # Extract the JSON string from the JavaScript content using regex
+                json_match = re.search(r'{.*}', script_content)
+                if json_match:
+                    json_string = json_match.group(0)
+
+                    # Convert the JSON string to a Python dictionary
+                    bookly_l10n_global = json.loads(json_string)
+
+                    # Extract csrf_token
+                    csrf_token = bookly_l10n_global.get('csrf_token')
+                else:
+                    csrf_token = None
+            else:
+                csrf_token = None
+
+            # Prepare response
+            response_data = {
+                'bookly_form_id': bookly_form_id.split('-')[-1],
+                'phpsessid_cookie': phpsessid_cookie,
+                'csrf_token': csrf_token,
+                "url": url
+            }
+
+            return jsonify(response_data), 200
+
+        else:
+            return jsonify(
+                {'error': f'Failed to fetch page. Status code: {response.status_code}'}), response.status_code
+
+
+@app.route('/', methods=['GET'])
+def home():
+    return "<h1>Distant Reading Archive</h1><p>This site is a prototype API for distant reading of science fiction novels.</p>"
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
